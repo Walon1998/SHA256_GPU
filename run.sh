6d79cc3b1f414052ec4f5735bbe2c15da06afce0
Neville Walo <walon@student.ethz.ch>
2020-11-28 22:35:35 +0100

    ready to benchmark

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 module load cuda
 module load gcc
+export CUDA_AUTO_BOOST=0
 nvcc main.cu -O3 -std=c++11 -gencode arch=compute_70,code=sm_70
 nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_1_1.csv ./a.out 1 1
 nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_64_80.csv ./a.out 64 80
