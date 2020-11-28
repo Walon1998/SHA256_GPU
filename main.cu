6a09e376134baf4e31dcd81c553fa1f6cddd016c
Neville Walo <walon@student.ethz.ch>
2020-11-28 14:28:47 +0100

    add input read

--- main.cu
+++ main.cu
@@ -2,9 +2,9 @@
 #include "sha256_on_gpu.h"
 
 
-int main() {
-    int threads = 1;
-    int blocks = 1;
+int main(int argc, char* argv[]) {
+    int threads = atoi(argv[1]);
+    int blocks = atoi(argv[2]);
     sha256_on_gpu_test();
     sha256_on_gpu_bench(threads,blocks);
 //    std::cout << "Hello, World!" << std::endl;
