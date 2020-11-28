ecf66388d0962c091806d1f3c556a933839569ee
Neville Walo <walon@student.ethz.ch>
2020-11-28 22:44:20 +0100

    fix

--- sha256_on_gpu.h
+++ sha256_on_gpu.h
@@ -94,10 +94,10 @@ void sha256_on_gpu_test() {
 
 void sha256_on_gpu_bench(const int threads = 1, const int blocks = 1) {
 
-//    for (int i = 0; i < 9; i++) {
-    std::cout << std::pow(10, 6) << ": <" << threads << ", " << blocks << ">" << std::endl;
-    sha256_on_gpu(std::string(std::pow(10, 6), 'a'), true, threads, blocks);
-//    }
+    for (int i = 0; i < 9; i++) {
+        std::cout << std::pow(10, i) << ": <" << threads << ", " << blocks << ">" << std::endl;
+        sha256_on_gpu(std::string(std::pow(10, i), 'a'), true, threads, blocks);
+    }
 
 }
 
