5a1809f83f5aeca74d15cbc04efddd87b2d0ff6e
Neville Walo <walon@student.ethz.ch>
2020-11-29 13:44:34 +0100

    fix

--- run.sh
+++ run.sh
@@ -4,14 +4,24 @@ module load cuda
 module load gcc
 export CUDA_AUTO_BOOST=0
 nvcc main.cu -O3 -std=c++11 -gencode arch=compute_70,code=sm_70
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_1_1.csv ./a.out 1 1
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_64_80.csv ./a.out 64 80
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_64_160.csv ./a.out 64 160
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_64_240.csv ./a.out 64 240
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_128_80.csv ./a.out 128 80
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_128_160.csv ./a.out 128 160
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_128_240.csv ./a.out 128 240
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_256_80.csv ./a.out 256 80
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_256_160.csv ./a.out 256 160
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_256_240.csv ./a.out 256 240
-nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_256_1024.csv ./a.out 256 1024
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 1 1
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 64 80
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 64 160
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 64 240
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 128 320
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 128 80
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 128 160
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 128 240
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 128 320
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 256 80
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 256 160
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 256 240
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 256 320
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 512 80
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 512 160
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 512 240
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 512 320
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 1024 80
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 1024 160
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 1024 240
+sbatch --nodelist=ault05,ault06 --time=04:00:00 --gres=gpu:1 -n1  --cpus-per-task=8  --mem-per-cpu=4096 ./run_single.sh 1024 320
--- run_single.sh
+++ run_single.sh
@@ -3,5 +3,5 @@
 module load cuda
 module load gcc
 export CUDA_AUTO_BOOST=0
-nvcc main.cu -O3 -std=c++11 -gencode arch=compute_70,code=sm_70
+#nvcc main.cu -O3 -std=c++11 -gencode arch=compute_70,code=sm_70
 nvprof --concurrent-kernels off --csv --profile-from-start off --print-gpu-trace --log-file Result/GPU_${1}_${2}.csv ./a.out $1 $2
